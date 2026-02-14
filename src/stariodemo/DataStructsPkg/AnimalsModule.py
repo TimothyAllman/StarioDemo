7f8dd0b7429024887ea45cb28b2d5b4d8a107902
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-02-14 12:09:53 +0200

    Create AnimalsModule.py

---
+++ src/stariodemo/DataStructsPkg/AnimalsModule.py
@@ -0,0 +1,25 @@
+
+ANIMALS = [
+    "Panda",
+    "Fox",
+    "Owl",
+    "Cat",
+    "Dog",
+    "Bear",
+    "Wolf",
+    "Tiger",
+    "Lion",
+    "Koala",
+    "Bunny",
+    "Penguin",
+    "Otter",
+    "Seal",
+    "Duck",
+    "Frog",
+    "Sloth",
+    "Deer",
+    "Moose",
+    "Falcon",
+]
+
+
