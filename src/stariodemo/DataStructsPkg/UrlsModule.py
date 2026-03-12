a3b55a262749047234b0e8383e6566aef1a60e32
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-03-12 18:07:25 +0200

    Update UrlsModule.py

--- src/stariodemo/DataStructsPkg/UrlsModule.py
+++ src/stariodemo/DataStructsPkg/UrlsModule.py
@@ -1,12 +1,12 @@
 HOME_PAGE_URL = "/"
 
-ABC_LIST_URL = "/abc/list/"
-ABC_ADD_URL = "/abc/add/"
+ABC_LIST_PAGE_URL = "/abc/list/"
+ABC_ADD_PAGE_URL = "/abc/add/"
 
-XYZ_LIST_URL = "/xyz/list/"
-XYZ_ADD_URL = "/xyz/add/"
+XYZ_LIST_PAGE_URL = "/xyz/list/"
+XYZ_ADD_PAGE_URL = "/xyz/add/"
 
-CHAT_URL = "/chat/"
+CHAT_PAGE_URL = "/chat/"
 SUBSCRIBE_URL = "/subscribe/"
 SEND_URL = "/send/"
 TYPING_URL = "/typing/"
