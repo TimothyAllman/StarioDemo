1edcf01cf9c4f535d12d735a035808e92577a716
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-03-18 18:28:19 +0200

    Create UserCreateViewModule.py

---
+++ src/stariodemo/HtmlViewsPkg/UserCreateViewModule.py
@@ -0,0 +1,42 @@
+from stario.html import Div
+from stario.html import Input
+
+from stariodemo.HtmlComponentsPkg.DemoAppButtonModule import DemoAppButton
+
+
+def UserCreateView():
+    """
+    docstring
+    """
+
+    return Div(
+        {"class": "mt-3 bg-gray-100 p-4"},  # {"class": "bg-gray-100 p-4 border border-gray-800"},
+        Div(
+            # {"class": "flex flex-row justify-between"},
+            Input(
+                {
+                    "id": "message-input",
+                    "type": "text",
+                    "class": "message-input",
+                    "placeholder": "Type a message...",
+                    "autocomplete": "off",
+                    "autofocus": True,
+                },
+                # data.bind("message"),
+                # data.on(
+                #     "keydown",
+                #     """
+                # if (evt.key === 'Enter' && !evt.shiftKey && $message.trim()) {
+                #     evt.preventDefault();
+                #     @post('/send');
+                #     $message = '';
+                # }
+                # """,
+                # ),
+                # data.on("input", at.post("/typing")),
+            ),
+            DemoAppButton(
+                "Create",
+            ),
+        ),
+    )
