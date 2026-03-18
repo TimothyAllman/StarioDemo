bd848d9eb30f9864a77330055ce8df240d7e47e9
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-03-18 18:33:09 +0200

    Update PageModule.py

--- src/stariodemo/HtmlComponentsPkg/PageModule.py
+++ src/stariodemo/HtmlComponentsPkg/PageModule.py
@@ -1,25 +1,12 @@
-import time
-
 from stario import asset
-from stario import at
-from stario import data
 from stario.html import Body
-from stario.html import Button
-from stario.html import Div
-from stario.html import Form
 from stario.html import Head
 from stario.html import Html
-from stario.html import Input
 from stario.html import Link
 from stario.html import Meta
-from stario.html import SafeString
 from stario.html import Script
-from stario.html import Span
 from stario.html import Title
 from stario.toys import toy_inspector
-
-from stariodemo.DataStructsPkg.MessageModule import Message
-from stariodemo.DataStructsPkg.UserModule import User
 
 
 def page(*children):
@@ -40,6 +27,7 @@ def page(*children):
             Script({"src": "https://cdn.tailwindcss.com"}),
         ),
         Body(
+            toy_inspector(),  # Dev tool: shows current signals state
             *children,
         ),
     )
