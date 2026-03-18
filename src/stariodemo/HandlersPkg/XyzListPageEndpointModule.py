9213e4977732a5bb5e168be8a898a49c6f0568e0
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-03-18 18:14:56 +0200

    Update XyzListPageEndpointModule.py

--- src/stariodemo/HandlersPkg/XyzListPageEndpointModule.py
+++ src/stariodemo/HandlersPkg/XyzListPageEndpointModule.py
@@ -1,21 +1,26 @@
 from stario import Context
 from stario import Writer
 
+from stariodemo.DataStructsPkg.UserModule import User
 from stariodemo.HtmlComponentsPkg.PageModule import page
 from stariodemo.HtmlViewsPkg.NavBarAndFooterViewModule import NavBarAndFooterView
 from stariodemo.HtmlViewsPkg.XyzListViewModule import XyzListView
 from stariodemo.HtmlViewsPkg.XyzSidebarViewModule import XyzSidebarView
 
 
-def XyzListPageEndpoint():
+def XyzListPageEndpoint(
+    Database: list[User],
+):
     async def handler(c: Context, w: Writer) -> None:
+
+        items = Database[1:-1]
 
         # Pass empty collections - user will get real data after subscribing
         w.html(
             page(
                 NavBarAndFooterView(
                     XyzSidebarView(
-                        XyzListView(),
+                        XyzListView(items),
                     )
                 )
             )
