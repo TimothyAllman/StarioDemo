2308c19a635714578fec4b50e64449705d30a682
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-03-18 18:15:36 +0200

    Update AbcCalculationViewModule.py

--- src/stariodemo/HtmlViewsPkg/AbcCalculationViewModule.py
+++ src/stariodemo/HtmlViewsPkg/AbcCalculationViewModule.py
@@ -1,34 +1,10 @@
-import time
-
-from stario import asset
 from stario import at
 from stario import data
-from stario.html import H1
-from stario.html import Button
 from stario.html import Div
-from stario.html import Form
-from stario.html import Head
-from stario.html import Html
-from stario.html import Input
-from stario.html import Link
-from stario.html import Meta
-from stario.html import SafeString
-from stario.html import Script
-from stario.html import Span
-from stario.html import Title
-from stario.toys import toy_inspector
 
-from stariodemo.DataStructsPkg.MessageModule import Message
-from stariodemo.DataStructsPkg.UserModule import User
-from stariodemo.HtmlComponentsPkg.FooterBarModule import FooterBar
-from stariodemo.HtmlViewsPkg.InputFormViewModule import input_form_view
-from stariodemo.HtmlViewsPkg.MessagesViewModule import messages_view
-from stariodemo.HtmlComponentsPkg.NavBarModule import NavBar
-from stariodemo.HtmlViewsPkg.OnlineUsersViewModule import online_users_view
-from stariodemo.HtmlComponentsPkg.PageModule import page
-from stariodemo.HtmlComponentsPkg.SideBarButtonModule import SideBarButton
-from stariodemo.HtmlComponentsPkg.SideBarModule import SideBar
-from stariodemo.HtmlViewsPkg.TypingIndicatorViewModule import typing_indicator_view
+from stariodemo.DataStructsPkg.UrlsModule import API_ABC_CALCULATION_URL
+from stariodemo.HtmlComponentsPkg.DemoAppButtonModule import DemoAppButton
+from stariodemo.HtmlViewsPkg.CalculationResultBoxViewModule import CalculationResultBoxView
 
 
 def AbcCalculationView(
@@ -43,4 +19,20 @@ def AbcCalculationView(
     docstring
     """
 
-    return Div("abc calc")
+    return Div(
+        Div("abc calc"),
+        Div(
+            {"class": ""},
+            DemoAppButton(
+                {"class": "border"},
+                data.on("click", at.get(API_ABC_CALCULATION_URL)),
+                "press me",
+            ),
+        ),
+        Div(
+            {"class": "mt-3"},
+            CalculationResultBoxView(
+                result=" initialised",
+            ),
+        ),
+    )
