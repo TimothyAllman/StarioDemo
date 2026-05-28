87e2b4c65c2535ea3828655f52224934f8bce93f
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-05-29 00:04:54 +0200

    Create AbcListHtmlModule.py

---
+++ src/stariodemo/HtmlHtmlsPkg/AbcListHtmlModule.py
@@ -0,0 +1,16 @@
+from stario.html import Div
+
+
+def AbcListHtml(
+    # user_id: str,
+    # username: str,
+    # color: str,
+    # *,
+    # messages: list[Message],
+    # users: dict[str, User],
+):
+    """
+    docstring
+    """
+
+    return Div("abc list")
