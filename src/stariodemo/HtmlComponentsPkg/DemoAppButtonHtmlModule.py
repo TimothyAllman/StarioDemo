23be89445b7a75fcb93f61de2cbe1de17adfae93
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-05-29 00:04:04 +0200

    Create DemoAppButtonHtmlModule.py

---
+++ src/stariodemo/HtmlComponentsPkg/DemoAppButtonHtmlModule.py
@@ -0,0 +1,16 @@
+from stario.html import Button
+
+
+def DemoAppButtonHtml(
+    *children,
+    buttoncolor="blue",
+):
+    return Button(
+        {
+            "class": [
+                f"text-white font-semibold px-4 py-4 bg-{buttoncolor}-700",
+                f"hover:bg-{buttoncolor}-800 transition transform hover:scale-105",
+            ]
+        },
+        *children,
+    )
