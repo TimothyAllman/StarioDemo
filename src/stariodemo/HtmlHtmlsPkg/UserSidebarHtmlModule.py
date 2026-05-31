dbc45da7fa3f09091ee37fb2855595c2af30b9f7
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-05-31 18:58:21 +0200

    add user html

---
+++ src/stariodemo/HtmlHtmlsPkg/UserSidebarHtmlModule.py
@@ -0,0 +1,22 @@
+from stariodemo.DataStructsPkg.UrlsModule import USER_ADD_PAGE_URL
+from stariodemo.DataStructsPkg.UrlsModule import USER_EDIT_PAGE_URL
+from stariodemo.DataStructsPkg.UrlsModule import USER_LIST_PAGE_URL
+from stariodemo.HtmlComponentsPkg.CommonSidebarLeftHtmlModule import CommonSidebarLeftHtml
+from stariodemo.HtmlComponentsPkg.SideBarButtonHtmlModule import SideBarButtonHtml
+
+
+def UserSideBarHtml(
+    *children,
+):
+    """
+    docstring
+    """
+
+    return CommonSidebarLeftHtml(
+        [
+            SideBarButtonHtml(name="List Users", url=USER_LIST_PAGE_URL),
+            SideBarButtonHtml(name="Add User", url=USER_ADD_PAGE_URL),
+            SideBarButtonHtml(name="Edit User", url=USER_EDIT_PAGE_URL),
+        ],
+        *children,
+    )
