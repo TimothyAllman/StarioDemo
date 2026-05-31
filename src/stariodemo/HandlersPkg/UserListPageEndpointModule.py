597cfad4243e6d5ba2dfc9f6bf822974446ef5ed
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-05-31 18:58:40 +0200

    add user pages endpoints

---
+++ src/stariodemo/HandlersPkg/UserListPageEndpointModule.py
@@ -0,0 +1,27 @@
+from stario import Context
+from stario import Writer
+from stario import responses
+
+from stariodemo.HtmlComponentsPkg.PageHtmlModule import PageHtml
+from stariodemo.HtmlHtmlsPkg.NavBarAndFooterHtmlModule import NavBarAndFooterHtml
+from stariodemo.HtmlHtmlsPkg.UserListHtmlModule import UserListHtml
+from stariodemo.HtmlHtmlsPkg.UserSidebarHtmlModule import UserSideBarHtml
+
+
+def UserListPageEndpoint():
+    async def handler(c: Context, w: Writer) -> None:
+        """
+        Serve abc list page
+        """
+        responses.html(
+            w,
+            PageHtml(
+                NavBarAndFooterHtml(
+                    UserSideBarHtml(
+                        UserListHtml(),
+                    )
+                )
+            ),
+        )
+
+    return handler
