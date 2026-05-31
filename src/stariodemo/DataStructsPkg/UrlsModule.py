65aeab4e6512f17e7596243f0b69209536604ff8
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-05-31 18:59:41 +0200

    remove trailing /

--- src/stariodemo/DataStructsPkg/UrlsModule.py
+++ src/stariodemo/DataStructsPkg/UrlsModule.py
@@ -1,26 +1,26 @@
 HOME_PAGE_URL = "/"
 
-ABC_LIST_PAGE_URL = "/abc/list/"
-ABC_ADD_PAGE_URL = "/abc/add/"
-ABC_CALCULATION_PAGE_URL = "/abc/calculation/"
+ABC_LIST_PAGE_URL = "/abc-list"
+ABC_ADD_PAGE_URL = "/abc-add"
+ABC_CALCULATION_PAGE_URL = "/abc-calculation"
 
-XYZ_LIST_PAGE_URL = "/xyz/list/"
-XYZ_ADD_PAGE_URL = "/xyz/add/"
+
+USER_LIST_PAGE_URL = "/user-list"
+USER_ADD_PAGE_URL = "/user-add"
+USER_EDIT_PAGE_URL = "/user-edit"
+USER_DETAILS_PAGE_URL = "/user-details"
+
 
 CHAT_PAGE_URL = "/chat/"
-SUBSCRIBE_URL = "/subscribe/"
-SEND_URL = "/send/"
-TYPING_URL = "/typing/"
+CHAT_SUBSCRIBE_URL = "/subscribe"
+CHAT_SEND_URL = "/send/"
+CHAT_TYPING_URL = "/typing"
 
-API_CALCULATION_URL = "/api/calculation/"
+
+API_CALCULATION_URL = "/api/calculation"
 API_USER_CREATE_URL = "/api/user/create"
-API_ABC_CALCULATION_URL = "/api/abc/calculation/"
+API_ABC_CALCULATION_URL = "/api/abc/calculation"
 
-API_WIDGET_LIST_URL = "/api/widget/list"
-API_WIDGET_ADD_URL = "/api/widget/create"
-API_WIDGET_SEED_URL = "/api/widget/seed"
 
-# API_ABC_CALCULATION_URL = "/api/abc/calculation/"
-
-GIVE_ME_TEXT_URL = "/givemetext/"
-GIVE_ME_JSON_URL = "/givemejson/"
+GIVE_ME_TEXT_URL = "/give-me-text"
+GIVE_ME_JSON_URL = "/give-me-json"
