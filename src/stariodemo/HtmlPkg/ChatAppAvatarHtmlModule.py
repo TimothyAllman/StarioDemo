bede8c1d9def325cb41faa68e26efa0c01409f5b
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-01 21:32:56 +0200

    Update ChatAppAvatarHtmlModule.py

--- src/stariodemo/HtmlPkg/ChatAppAvatarHtmlModule.py
+++ src/stariodemo/HtmlPkg/ChatAppAvatarHtmlModule.py
@@ -9,9 +9,7 @@ def ChatAppAvatarHtml(
     return Span(
         {
             "class": "avatar w-7 h-7 rounded-full flex items-center justify-center text-[0.7rem] font-semibold text-white border-2 border-surface -ml-2 cursor-default uppercase shadow-sm last:ml-0",
-            "style": {
-                "background-color": avatar_color,
-            },
+            "style": f"background-color: {avatar_color}",
             "title": avatar_title,
         },
         avatar_text,
