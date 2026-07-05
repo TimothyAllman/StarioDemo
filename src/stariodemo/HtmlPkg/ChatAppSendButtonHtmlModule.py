4581791226c97d29eba5f9392b23e6f01a81302b
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-06 00:23:05 +0200

    Update ChatAppSendButtonHtmlModule.py

--- src/stariodemo/HtmlPkg/ChatAppSendButtonHtmlModule.py
+++ src/stariodemo/HtmlPkg/ChatAppSendButtonHtmlModule.py
@@ -10,9 +10,9 @@ def ChatAppSendButtonHtml(
         {
             "class": " ".join(
                 [
-                    "send-button w-[42px] h-[42px] rounded-full border-0 bg-linear-to-br from-amber-500 to-amber-400 text-white cursor-pointer flex items-center justify-center",
-                    "transition duration-150 shadow-blue-600 shrink-0",
-                    "hover:enabled:scale-105 hover:enabled:shadow-blue-900",
+                    "send-button w-[42px] h-[42px] rounded-full border-0 bg-linear-to-br from-backcolor1 to-backcolor1 text-frontcolor2 cursor-pointer flex items-center justify-center",
+                    "transition duration-150 shadow-backcolor5 shrink-0",
+                    "hover:enabled:scale-105 hover:enabled:shadow-backcolor4",
                     "active:enabled:scale-95 disabled:opacity-40 disabled:cursor-not-allowed disabled:shadow-none",
                 ]
             )
