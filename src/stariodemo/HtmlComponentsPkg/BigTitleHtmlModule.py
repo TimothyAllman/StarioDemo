11a9999061ade09606fca923e20cb1d5e64cb33b
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-06 00:26:30 +0200

    Update BigTitleHtmlModule.py

--- src/stariodemo/HtmlComponentsPkg/BigTitleHtmlModule.py
+++ src/stariodemo/HtmlComponentsPkg/BigTitleHtmlModule.py
@@ -6,7 +6,7 @@ def BigTitleHtml(title: str):
         {
             "class": " ".join(
                 [
-                    "text-3xl font-bold tracking-tight text-slate-900",
+                    "text-3xl font-bold tracking-tight text-frontcolor4",
                     "mb-4",
                 ]
             )
