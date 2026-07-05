5c149898cd5ea6b3d948a9015e24415f145d3058
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-05 21:58:52 +0200

    Update ChatAppMessageInputHtmlModule.py

--- src/stariodemo/HtmlPkg/ChatAppMessageInputHtmlModule.py
+++ src/stariodemo/HtmlPkg/ChatAppMessageInputHtmlModule.py
@@ -13,9 +13,9 @@ def ChatAppMessageInputHtml(
         {
             "class": " ".join(
                 [
-                    "message-input flex-1 bg-bg border border-border-light rounded-[10px] px-4 py-[0.7rem] text-[0.9rem] text-fg outline-non transition-[border-color,box-shadow] duration-150",
-                    "focus:border-amber-300 focus:ring-[3px] focus:ring-blue-600",
-                    "placeholder:text-muted",
+                    "message-input flex-1 bg-backcolor1 border border-edgecolor1 rounded-[10px] px-4 py-[0.7rem] text-[0.9rem] text-frontcolor1 outline-non transition-[border-color,box-shadow] duration-150",
+                    "focus:border-edgecolor5 focus:ring-[3px] focus:ring-backcolor5",
+                    "placeholder:text-frontcolor5",
                 ]
             )
         },
