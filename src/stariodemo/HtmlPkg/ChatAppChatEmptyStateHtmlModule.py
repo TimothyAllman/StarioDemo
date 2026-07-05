6fe8fffee2467cae4984ea96f1e53cdd3613b63f
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-06 00:29:48 +0200

    Update ChatAppChatEmptyStateHtmlModule.py

--- src/stariodemo/HtmlPkg/ChatAppChatEmptyStateHtmlModule.py
+++ src/stariodemo/HtmlPkg/ChatAppChatEmptyStateHtmlModule.py
@@ -6,7 +6,7 @@ def ChatAppChatEmptyStateHtml():
         {
             "class": " ".join(
                 [
-                    "empty-state text-muted text-[0.9rem] p-8 text-center bg-surface rounded-[10px] border border-border-light",
+                    "empty-state text-frontcolor1 text-[0.9rem] p-8 text-center bg-backcolor1 rounded-[10px] border border-edgecolor1",
                 ]
             )
         },
