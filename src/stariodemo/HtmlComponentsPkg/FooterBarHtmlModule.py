07a7b8018bf1caf4a5313d672b26d9755e2a50c2
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-05 20:33:23 +0200

    Update FooterBarHtmlModule.py

--- src/stariodemo/HtmlComponentsPkg/FooterBarHtmlModule.py
+++ src/stariodemo/HtmlComponentsPkg/FooterBarHtmlModule.py
@@ -1,16 +1,21 @@
 from stario.markup.html import H1
 from stario.markup.html import Div
 
+from stariodemo.HtmlComponentsPkg.ThemeSelectorHtmlModule import ThemeSelectorHtml
+
 
 def FooterBarHtml():
     return Div(
         {"class": "bg-color11 p-4 flex items center justify-between min-h-60"},
         {"id": "idFooter"},
+        ThemeSelectorHtml(),
+        ThemeSelectorHtml(),
         Div(
             {"class": "flex items-center space-x-6"},
             H1(
-                {"class": "text-white text-2xl font-bold"},
+                {"class": "text-fg text-2xl font-bold"},
                 "Thanks",
             ),
         ),
+        ThemeSelectorHtml(),
     )
