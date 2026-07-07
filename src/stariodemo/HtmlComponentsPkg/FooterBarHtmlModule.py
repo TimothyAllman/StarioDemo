017eab0173c3873c37680799adc2d17b8d62f5c0
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-07-08 00:59:56 +0200

    Update FooterBarHtmlModule.py

--- src/stariodemo/HtmlComponentsPkg/FooterBarHtmlModule.py
+++ src/stariodemo/HtmlComponentsPkg/FooterBarHtmlModule.py
@@ -1,4 +1,5 @@
 from stario.markup.html import H1
+from stario.markup.html import A
 from stario.markup.html import Div
 
 from stariodemo.HtmlComponentsPkg.ThemeSelectorHtmlModule import ThemeSelectorHtml
@@ -8,14 +9,41 @@ def FooterBarHtml():
     return Div(
         {"class": "bg-backcolor1 p-4 flex items center justify-between min-h-60"},
         {"id": "idFooter"},
-        ThemeSelectorHtml(),
-        ThemeSelectorHtml(),
-        Div(
-            {"class": "flex items-center space-x-6"},
-            H1(
-                {"class": "text-frontcolor1 text-2xl font-bold"},
-                "Thanks",
-            ),
+        FooterOtherLinks(),
+        FooterColumn(
+            *[
+                "About",
+                "Contact Us",
+            ],
+            title="Thanks",
         ),
-        ThemeSelectorHtml(),
+        FooterColumn(
+            ThemeSelectorHtml(),
+            title="Theme",
+        ),
+    )
+
+
+def FooterColumn(*children, title):
+    return Div(
+        {"class": "flex flex-col"},
+        H1(
+            {"class": "text-frontcolor1 text-2xl font-bold"},
+            title,
+        ),
+        *children,
+    )
+
+
+def FooterOtherLinks():
+    return FooterColumn(
+        H1(
+            "OtherLinks",
+        ),
+        [
+            A("link 1"),
+            A("link 4"),
+            A("link 3"),
+        ],
+        title="Other Links",
     )
