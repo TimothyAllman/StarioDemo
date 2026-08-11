e470f2b0002f5636841c5b7f1fa777adba3b4581
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-08-11 22:39:17 +0200

    Update UrlsModule.py

--- src/stariodemo/BasicStructsPkg/UrlsModule.py
+++ src/stariodemo/BasicStructsPkg/UrlsModule.py
@@ -8,6 +8,7 @@ ABC_CALCULATION_PAGE_URL = UrlPath("/abc-calculation")
 
 
 WIDGET_LIST_PAGE_URL = UrlPath("/widget-list")
+WIDGET_LIST_API_URL = UrlPath("/widget-list-api")
 WIDGET_ADD_PAGE_URL = UrlPath("/widget-add")
 WIDGET_EDIT_PAGE_URL = UrlPath("/widget-edit")
 WIDGET_DETAILS_PAGE_URL = UrlPath("/widget-details")
