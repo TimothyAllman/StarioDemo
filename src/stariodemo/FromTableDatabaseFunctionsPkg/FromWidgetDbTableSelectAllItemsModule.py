2a12eb090b451c29c9f718e46991bc2f9653113a
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-08-11 22:39:32 +0200

    Update FromWidgetDbTableSelectAllItemsModule.py

--- src/stariodemo/FromTableDatabaseFunctionsPkg/FromWidgetDbTableSelectAllItemsModule.py
+++ src/stariodemo/FromTableDatabaseFunctionsPkg/FromWidgetDbTableSelectAllItemsModule.py
@@ -2,8 +2,22 @@ from stariodemo.DatabasePiccoloTablesPkg.WidgetDbModule import WidgetDb
 from stariodemo.DatabasePiccoloTablesPkg.WidgetDbModule import WidgetListDto
 
 
-async def FromWidgetDbTableSelectAllItems() -> list[WidgetListDto] | None:
+async def FromWidgetDbTableSelectAllItems(
+    name_filter: str | None = None,
+    status_filter: str | None = None,
+) -> list[WidgetListDto]:
+
     qry = WidgetDb.select()
+
+    if name_filter:
+        qry = qry.where(
+            WidgetDb.name.like(f"%{name_filter}%"),
+        )
+
+    if status_filter:
+        qry = qry.where(
+            WidgetDb.name.like(f"%{status_filter}%"),
+        )
 
     rows = await qry.run()
 
