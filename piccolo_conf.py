308fb14777b4945aa80c3c6cf315a55d84ea82dd
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2026-08-13 19:16:59 +0200

    Update piccolo_conf.py

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,38 +1,21 @@
+import os
+
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.sqlite import SQLiteEngine
 
 from stariodemo.DatabasePiccoloTablesPkg import piccolo_app
 
-SQLITE_DB_PATH = "piccolochat.db"
+SQLITE_DB_PATH = os.getenv("LITESTREAM_DB_PATH", "no ENV_VAR found")
 
 DB = SQLiteEngine(
     path=SQLITE_DB_PATH,
     timeout=60,
-    
 )
 
-import asyncio
-from piccolo.querystring import QueryString
-from piccolo_conf import DB  
-
-async def enable_wal():
-    # 1. Establish the connection pool/engine
-    await DB.start_connection_pool()
-    
-    # 2. Wrap the PRAGMA statements in Piccolo QueryStrings
-    wal_query = QueryString("PRAGMA journal_mode=WAL;")
-    sync_query = QueryString("PRAGMA synchronous=NORMAL;")
-    
-    # 3. Execute the queries directly through the engine instance
-    await DB.run_querystring(wal_query)
-    await DB.run_querystring(sync_query)
-    
-    # 4. Clean up connection pool (if running as a standalone script)
-    await DB.close_connection_pool()
 
 APP_REGISTRY = AppRegistry(
     apps=[
-        #"stariodemo.DatabasePiccoloFunctionsPkg.piccolo_app",
+        # "stariodemo.DatabasePiccoloFunctionsPkg.piccolo_app",
         piccolo_app.__name__
     ]
 )
